f61458b7ebdb9433e6c47b0a3b0519c46033b8d2
Juha Kangas <juhakangas5@gmail.com>
2020-12-21 15:41:47 +0200

    Modified passport

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: 'http://stonksacademy.herokuapp.com/auth/google/callback',
         profileFields: ['id', 'emails', 'name'],
         prompt: 'select_account',
       },
