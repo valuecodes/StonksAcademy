be49b62080f59f8882ca136a272a2720d9d5df94
Juha Kangas <juhakangas5@gmail.com>
2021-01-14 18:48:32 +0200

    Added FinancialRatios course priceRatios section PB-Ratio article

--- client/src/components/Example/ResultCard.js
+++ client/src/components/Example/ResultCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Card from '@material-ui/core/Card';
+import { InfoTooltip } from '../Other/Tooltip';
 
-export default function ResultCard({header,value}) {
+export default function ResultCard({header,value,color='',tooltip=false}) {
     return (
-        <Card className='card'>
-            <h2>{header}</h2>
+        <Card className='card' style={{backgroundColor:color}}>
+            <h2>{header} {tooltip && <InfoTooltip text={tooltip}/>} </h2> 
             <div className='resultBig'>
                 {value}
             </div>
--- client/src/courses/courseMaterial/financialRatios/Section1.js
+++ client/src/courses/courseMaterial/financialRatios/Section1.js
@@ -11,7 +11,8 @@ import ResultCard from '../../../components/Example/ResultCard'
 export default function PriceRatios(props) {
     const sectionComponents = [
         {name:'OverView',article: OverView},
-        {name:'PE-Ratio',article:PERatio}
+        {name:'PE-Ratio',article:PERatio},
+        {name:'PB-Ratio',article:PBRatio},
     ]
     
     return (
@@ -22,13 +23,124 @@ export default function PriceRatios(props) {
     )
 }
 
+function PBRatio(){
+
+    const [pb, setPB] = useState({
+        sharePrice:80,
+        bvps:80,
+        pb:1,
+        liquid:100
+    })
+
+    function changeInputHandler(e,value,name){
+        const pbCopy = {...pb}
+        pbCopy[name]=+value
+        pbCopy.pb = +(pbCopy.sharePrice / pbCopy.bvps).toFixed(1)
+        pbCopy.liquid = ((pbCopy.bvps / pbCopy.sharePrice)*100).toFixed(1)
+        setPB(pbCopy)
+    }
+
+    const textListContent=[
+        {
+            text:"Price to Book ratio also known as PB-Ratio is used to measure company book value to share price. Book value gives idea of the valuation based on the company equity",
+            formula:'PB = Share price / Book Value per Share'
+        },
+        {
+            header:'Pros and Cons',
+            prosAndCons:{
+                pros:['Is more stable than PE','Works with negative earnings','Can be useful with companies going out of business'],
+                cons:['Works only with asset heavy sectors',"Doesn't work asset light sectors like tech ","Doesn't tell anything about debt/cash"]
+            },
+        },
+    ]
+
+    const handleGetColor = (ratio) => {
+        if(ratio<=1) return 'var(--positive-color)'
+        if(ratio<2) return 'var(--neutral-color)'
+        if(ratio>=2) return 'var(--negative-color)'
+    }
+
+    return(
+        <div className='sectionGrid3'>
+            <div>
+                <h2>PB-ratio</h2>
+                <TextList content={textListContent}/>
+            </div>
+            <div>
+                <ul className='shareInputs'>
+                    <li>
+                        <h2>Share Price</h2>
+                        <div className='sharePriceInput'>
+                            <TextField
+                                onChange={(e,value)=>changeInputHandler(e,e.target.value,'sharePrice')} 
+                                className='shareInput' 
+                                id="standard-basic" 
+                                label="" 
+                                type="number"
+                                name='sharePrice'
+                                value={pb.sharePrice}
+                            />
+                            <p>$</p>
+                            <InputSlider 
+                                onChange={changeInputHandler}
+                                value={pb.sharePrice}
+                                max={150}
+                                min={1}
+                                step={1}
+                                name='sharePrice'
+                                className='shareInputSlider' 
+                            />
+                        </div>                    
+                    </li>
+                    <li>
+                        <h2>BVPS</h2>
+                        <div className='sharePriceInput'>
+                            <TextField 
+                                onChange={(e,value)=>changeInputHandler(e,e.target.value,'bvps')} 
+                                className='shareInput'
+                                id="standard-basic" label="" type="number"
+                                name='bvps'
+                                value={pb.bvps}
+                            />     
+                            <p></p>
+                            <InputSlider
+                                onChange={changeInputHandler}
+                                value={pb.bvps}
+                                max={200}
+                                min={0}
+                                step={1}
+                                name='bvps'
+                                className='shareInputSlider' 
+                            />
+                        </div>
+                    </li>
+                </ul>                
+            </div>
+
+            <div>
+                <ResultCard 
+                    header={'PB-ratio'} 
+                    value={pb.pb} 
+                    color={handleGetColor(pb.pb)}
+                />
+                <ResultCard 
+                    header={'Liquidation value'} 
+                    value={pb.liquid+'%'} 
+                    color={handleGetColor(pb.pb)}
+                    tooltip={`In case of company liquidation\nThis is how many percentage you would get back from the initial investment\n\nPB 1\t100%\nPB 2\t50% \nPB 0.5\t200%`}
+                />
+            </div>
+        </div>
+    )
+}
+
 function PERatio(){
 
     const [pe, setPE] = useState({
-        sharePrice:80,
-        eps:4,
-        pe:20,
-        yield:5,
+        sharePrice:120,
+        eps:8,
+        pe:15,
+        yield:6.7,
     })
 
     function changeInputHandler(e,value,name){
@@ -48,10 +160,17 @@ function PERatio(){
             header:'Pros and Cons',
             prosAndCons:{
                 pros:['Easy to Calculate','Quick estimation of the expexted return','Helps at comparing diffent companies'],
-                cons:['Is based on the past earnings','Can be manipulated by accounting practises',"Doesn't tell anything about debt/cash"]
+                cons:['Is based on the past earnings','Can be manipulated by accounting practises',"Doesn't tell anything about debt/cash","Doesn't work with negative earnings"]
             }
         },
     ]
+
+    const handleGetColor = (ratio) => {
+        if(ratio>=25||ratio<0) return 'var(--negative-color)'
+        if(ratio<=15) return 'var(--positive-color)'
+        if(ratio<25) return 'var(--neutral-color)'
+        
+    }
 
     return(
         <div className='sectionGrid3'>
@@ -114,10 +233,12 @@ function PERatio(){
                 <ResultCard 
                     header={'PE-ratio'} 
                     value={pe.pe} 
+                    color={handleGetColor(pe.pe)}
                 />
                 <ResultCard 
                     header={'Earnings Yield'} 
                     value={`${pe.yield}%`} 
+                    color={handleGetColor(pe.pe)}
                 />
             </div>
         </div>
